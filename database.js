8adaad02026f21693994124f903c583ad05930d2
Asher De Souza <asher.desouza@clearscore.com>
2025-04-09 22:50:42 +0100

    ssl

--- database.js
+++ database.js
@@ -2,6 +2,6 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}?ssl=true`);
 
 export default sequelize;
